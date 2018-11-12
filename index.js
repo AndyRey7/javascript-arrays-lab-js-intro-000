cd339144ca3e7f83a58d5264c455518d918b0e3d
andy reyes <11710reyes@gmail.com>
2018-11-12 03:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten() {
+  kittens.push('Ralph')
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift('Bob')
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  kittens.slice('Broom')
+  return kittens
+}
